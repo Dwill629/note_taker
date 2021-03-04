cd980de33ff59af0dc47f1c02e96f32772eb2d53
Devon <Dwill629@gmail.com>
2021-03-04 00:00:53 -0500

    added app.get to server.js

--- server.js
+++ server.js
@@ -8,3 +8,12 @@ let dataB = require('./db/db.json')
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')))
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')))
+app.get('/api/notes', (req, res) => fs.readFile(path.join(__dirname, '/db/db.json'), (err, data) => {
+  if (err) throw err;
+  res.send(data)
+}))
